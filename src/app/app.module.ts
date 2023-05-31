c3eb24523197626419b10725ee1e5167122ad8d1
Timofey Tsepennikov <tta@biznpro.ru>
2023-06-01 02:33:22 +0500

    change app new bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductionCapacityComponent } from './pages/production-capacity/production-capacity.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { VacanciesComponent } from './pages/vacancies/vacancies.component';
+import { NewsComponent } from './pages/news/news.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+
+const appRoutes: Routes =[
+  { path: '', component: MainComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'production', component: ProductionCapacityComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'vacancies', component: VacanciesComponent},
+  { path: 'news', component: NewsComponent},
+  { path: 'contacts', component: ContactsComponent},
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    AboutComponent,
+    ProductionCapacityComponent,
+    ProjectsComponent,
+    VacanciesComponent,
+    NewsComponent,
+    ContactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/production-capacity/production-capacity.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-production-capacity',
+  templateUrl: './production-capacity.component.html',
+  styleUrls: ['./production-capacity.component.css']
+})
+export class ProductionCapacityComponent {
+
+}
